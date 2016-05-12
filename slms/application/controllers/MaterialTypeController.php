f547dd7cffdc8d708f5ba7249335077a154d9372
InasAbdelfattah <engenas21@gmail.com>
2016-05-12 06:59:44 +0200

    materialType[model,controller],material[model,controller],comment[model,controller,form]

---
+++ slms/application/controllers/MaterialTypeController.php
@@ -0,0 +1,40 @@
+<?php
+
+class MaterialTypeController extends Zend_Controller_Action
+{
+    private $model = null;
+    public function init()
+    {
+        /* Initialize action controller here */
+        $this->model = new Application_Model_MaterialTypes;
+    }
+
+ /*       public function indexAction() {
+//         $this->model = new Application_Model_Courses;
+        $types = $this->model->listMaterialTypes();
+        var_dump($types);
+        // action body
+    }*/
+
+    public function indexAction()
+    {
+        // action body
+        $course_id=$this->getRequest()->getParam('course_id');
+        
+        $materials = new Application_Model_Materials;
+        #select material_type_id from materials where course_id=$course_id
+
+        $m=$materials->fetchAll($materials->select('*')->where("course_id=?","$course_id"))->toArray();
+        #$this->view->m = $m ;
+        #select types from Materialtypes where id=$material_type_id
+        $types=[];
+        foreach ($m as $value) {
+            $type_id=$value['material_type_id']; 
+            $types=$this->model->fetchAll($this->model->select('*')->where("id=?","$type_id"))->toArray();
+        }
+         $this->view->types = $types ;
+        $this->view->course_id=$course_id;
+    }
+}
+
+?>
--- slms/application/models/Comments.php
+++ slms/application/models/Comments.php
@@ -2,10 +2,47 @@
 
 class Application_Model_Comments extends Application_Model_MyModel
 {
-    protected $_name = '';
+    protected $_name = 'comments';
     protected $primary_key = "id";
-    protected $fields = array();
-    
+    protected $fields = array('comment', 'user_id', 'material_id', 'created_at');
+    public $comment ;
+    public $user_id ;
+    public $material_id ;
 
+
+    function listComments() {
+        return $this->list_data();
+     
+    }
+
+    function getComment() {
+        return $this->get_id();
+       
+    }
+
+   /* function addComment() {
+        return $this->add_data();
+    }*/
+
+    function deleteComment() {
+        return $this->delete_id();
+    }
+
+    function updateComment() {
+        return $this->edit();
+    }
+
+  /*  	function addComment($commentInfo){
+		$row = $this->createRow();
+        #$row->user_id = $id;
+		$row->user_id = 2;
+		$row->material_id = $commentInfo['material_id'];
+		$row->comment = $commentInfo['comment'];
+
+		return $row->save();
+	}*/
+function addComment() {
+        return $this->add_data();
+    }
 }
 
